0372417b36dd9b37da17cd0752db90c04f994242
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-11-21 23:17:57 +0300

    completed addItem and selectItem methods, added decline function

--- src/store.js
+++ src/store.js
@@ -2,78 +2,84 @@
  * Хранилище состояния приложения
  */
 class Store {
-  constructor(initState = {}) {
-    this.state = initState;
-    this.listeners = []; // Слушатели изменений состояния
-  }
+	constructor(initState = {}) {
+		this.state = initState;
+		this.listeners = []; // Слушатели изменений состояния
+	}
 
-  /**
-   * Подписка слушателя на изменения состояния
-   * @param listener {Function}
-   * @returns {Function} Функция отписки
-   */
-  subscribe(listener) {
-    this.listeners.push(listener);
-    // Возвращается функция для удаления добавленного слушателя
-    return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
-  }
+	/**
+	 * Подписка слушателя на изменения состояния
+	 * @param listener {Function}
+	 * @returns {Function} Функция отписки
+	 */
+	subscribe(listener) {
+		this.listeners.push(listener);
+		// Возвращается функция для удаления добавленного слушателя
+		return () => {
+			this.listeners = this.listeners.filter((item) => item !== listener);
+		};
+	}
 
-  /**
-   * Выбор состояния
-   * @returns {Object}
-   */
-  getState() {
-    return this.state;
-  }
+	/**
+	 * Выбор состояния
+	 * @returns {Object}
+	 */
+	getState() {
+		return this.state;
+	}
 
-  /**
-   * Установка состояния
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
-  }
+	/**
+	 * Установка состояния
+	 * @param newState {Object}
+	 */
+	setState(newState) {
+		this.state = newState;
+		// Вызываем всех слушателей
+		for (const listener of this.listeners) listener();
+	}
 
-  /**
-   * Добавление новой записи
-   */
-  addItem() {
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+	/**
+	 * Добавление новой записи
+	 */
+	addItem(initialCode = 1) {
+		this.state.codeCounter = this.state.codeCounter ?? initialCode;
+		this.setState({
+			...this.state,
+			list: [...this.state.list, { code: this.state.codeCounter, title: "Новая запись", selectionCount: 0 }],
+		});
+		this.state.codeCounter++;
+	}
 
-  /**
-   * Удаление записи по коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+	/**
+	 * Удаление записи по коду
+	 * @param code
+	 */
+	deleteItem(code) {
+		this.setState({
+			...this.state,
+			list: this.state.list.filter((item) => item.code !== code),
+		});
+	}
 
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
-        }
-        return item;
-      })
-    })
-  }
+	/**
+	 * Выделение записи по коду
+	 * @param code
+	 */
+	selectItem(code) {
+		this.setState({
+			...this.state,
+			list: this.state.list.map((item) => {
+				item.selectionCount = item.selectionCount ?? 0;
+				if (item.code === code) {
+					item.selected = !item.selected;
+					item.selectionCount = item.selected ? item.selectionCount + 1 : item.selectionCount;
+				} else {
+					item.selected = false;
+				}
+				return item;
+			}),
+		});
+	}
 }
 
 export default Store;
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const propNames = new Set(['id', 'className', 'textContent', 'onclick']);
+const propNames = new Set(["id", "className", "textContent", "onclick"]);
 
 /**
  * Создание элемента со свойствами и вложенными элементами
@@ -8,21 +8,31 @@ const propNames = new Set(['id', 'className', 'textContent', 'onclick']);
  * @returns {HTMLElement}
  */
 export function createElement(name, props = {}, ...children) {
-  const element = document.createElement(name);
+	const element = document.createElement(name);
 
-  // Назначение свойств и атрибутов
-  for (const name of Object.keys(props)) {
-    if (propNames.has(name)) {
-      element[name] = props[name];
-    } else {
-      element.setAttribute(name, props[name]);
-    }
-  }
+	// Назначение свойств и атрибутов
+	for (const name of Object.keys(props)) {
+		if (propNames.has(name)) {
+			element[name] = props[name];
+		} else {
+			element.setAttribute(name, props[name]);
+		}
+	}
 
-  // Вставка вложенных элементов
-  for (const child of children) {
-    element.append(child);
-  }
+	// Вставка вложенных элементов
+	for (const child of children) {
+		element.append(child);
+	}
 
-  return element;
+	return element;
+}
+
+export function decline(count, declineOptions) {
+	const many = count % 100;
+	const few = count % 10;
+
+	if (many > 10 && many < 15) return declineOptions.many;
+	if (few > 1 && few < 5) return declineOptions.few;
+	if (few === 1) return declineOptions.single;
+	return declineOptions.many;
 }
