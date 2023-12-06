508c0cc04d6c9d886d6a530c27c68f50d4929a27
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-12-07 00:00:23 +0300

    installed and configured react router

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,7 @@
-import {useCallback, useContext, useEffect, useState} from 'react';
+import { useCallback, useContext, useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./main";
+import ArticlePage from "./article";
 import Basket from "./basket";
 import useStore from "../store/use-store";
 import useSelector from "../store/use-selector";
@@ -9,14 +11,16 @@ import useSelector from "../store/use-selector";
  * @returns {React.ReactElement}
  */
 function App() {
-
-  const activeModal = useSelector(state => state.modals.name);
+  const activeModal = useSelector((state) => state.modals.name);
 
   return (
-    <>
-      <Main/>
-      {activeModal === 'basket' && <Basket/>}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/article/:articleId" element={<ArticlePage />} />
+      </Routes>
+      {activeModal === "basket" && <Basket />}
+    </BrowserRouter>
   );
 }
 
