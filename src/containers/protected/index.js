fc59dd5b1a735e7b198a343df91c0e732d123642
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-12-21 21:16:21 +0300

    updated styles and added new translations

--- src/containers/protected/index.js
+++ src/containers/protected/index.js
@@ -1,26 +1,30 @@
-import {memo, useEffect} from 'react';
-import PropTypes from 'prop-types';
-import useSelector from '../../hooks/use-selector';
-import {useLocation, useNavigate} from 'react-router-dom';
+import { memo, useEffect } from "react";
+import PropTypes from "prop-types";
+// import useSelector from '../../hooks/use-selector';
+import { useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import shallowequal from "shallowequal";
 
-function Protected({children, redirect}) {
-
-  const select = useSelector(state => ({
-    exists: state.session.exists,
-    waiting: state.session.waiting
-  }));
+function Protected({ children, redirect }) {
+  const select = useSelector(
+    (state) => ({
+      exists: state.session.exists,
+      waiting: state.session.waiting,
+    }),
+    shallowequal
+  );
 
   const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
     if (!select.exists && !select.waiting) {
-      navigate(redirect, {state: {back: location.pathname}});
+      navigate(redirect, { state: { back: location.pathname } });
     }
   }, [select.exists, select.waiting]);
 
   if (!select.exists || select.waiting) {
-    return <div>Ждём...</div>
+    return <div>Ждём...</div>;
   } else {
     return children;
   }
@@ -29,6 +33,6 @@ function Protected({children, redirect}) {
 Protected.propTypes = {
   redirect: PropTypes.string,
   children: PropTypes.node,
-}
+};
 
 export default memo(Protected);
